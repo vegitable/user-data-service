0e557a2ee1c89a1ca3cbc37ac3f36b0c0021874f
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-10-31 11:25:53 +0000

    adds user model

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 1024
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(1024).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+module.exorts = {
+  User,
+  validateUser,
+}
--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,6 @@ const config = require('config');
 
 module.exports = function() {
   db.connect(config.get('db'))
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(() => console.log('Unable to connect to MongoDB...'));
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(() => console.log('Unable to connect to MongoDB...'));
 }
