0cd1a6f622ae3f27b0d1ee2f4ef617b2075a247b
timothy.hawkins <timothy.hawkins@kurtgeiger.com>
2018-10-31 11:01:26 +0000

    adds MongoDB setup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const db = require('mongoose');
+
+db.connect('mongodb://testuser:testuser1@ds016108.mlab.com:16108/user-data-service')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(() => console.log('Unable to connect to MongoDB...'));
 
 app.get('/', (req, res) => {
   res.send('Hello user data service!');
