86b918b1022f03c97987fdb837ee3700128ec58c
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-01-24 14:27:08 +0000

    reverts to process.env for cors header

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', process.env.CORSHEADER);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
   res.setHeader('Access-Control-Allow-Credentials', true);
