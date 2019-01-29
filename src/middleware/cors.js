3620ba22cedefd3ff73572cabbb3ee417eda07fd
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-01-29 13:39:13 +0000

    cors to eslint

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -4,4 +4,4 @@ module.exports = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, x-access-token');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
-}
+};
