f8a611365dcff4f8620485cc8491ad0bedbea2b6
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-01-30 12:47:11 +0000

    adds test files to eslintignore

---
+++ tests/integration/profiles.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const { app } = require('../../src/index');
+
+describe('/api/profiles', () => {
+  beforeEach(() => {
+  });
+
+  afterEach(() => {
+  });
+
+  describe('GET /api/profiles/getRestaurants', () => {
+    it('should return a status of 200 when a new user is created', (done) => {
+
+    });
+  });
+});
